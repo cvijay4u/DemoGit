c40e283e0238b60e1df5a8742f145e5d15455d26
cvijay4u <cvijay4u@gmail.com>
2023-02-20 20:41:39 +0530

    updated demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
 print("welcome  git demo class ") -> writen  @VS code
 print("update in github") -> writen @github
+# condition  for  merge conflict is  that ,changes should  occour in  the  same file and  same line
+print("Line1")
+print("Line2")
+print("Line3")
+print("Line4")
+print("Line5")
